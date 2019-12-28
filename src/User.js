dbd96ab7c70cd289e8b7d5452a0c3007d35066be
Tiffany White <hey@tiffanyrwhite.com>
2019-12-28 13:40:02 -0500

    Removed lifecycle method from User component

--- src/User.js
+++ src/User.js
@@ -2,19 +2,6 @@ import React from 'react'
 
 class User extends React.Component {
 
-  state = {
-    userData: []
-  }
-  render() {
-    componentDidMount() {
-    fetch('https://faker-api.glitch.me/api/user')
-      .then(res => res.json())
-      .then((data) => {
-        this.setState({ userData: data })
-      })
-      .catch(console.log)
-    }
-  }
 }
 
 
