86012c8b62a1aa4f9c4a484e01ee5fdc74032b91
Tiffany White <hey@tiffanyrwhite.com>
2019-12-28 21:40:08 -0500

    Add API, finish out App component. Still needs work

--- src/App.js
+++ src/App.js
@@ -1,38 +1,66 @@
 import React from 'react';
 import './App.css';
-import { Form, FormInput, FormGroup } from 'shards-react';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'shards-ui/dist/css/shards.min.css'
+import Loader from 'react-loader-spinner'
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
 class App extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  // }
-  state = {
-    userData: []
+  constructor(props) {
+    super(props);
+    this.state = {
+      userData: [],
+      isLoaded: false
+      // isClicked: true
+    }
   }
+
   componentDidMount() {
     fetch('https://faker-api.glitch.me/api/user')
       .then(res => res.json())
-      .then((data) => {
-        this.setState({ userData: data })
-      })
-      .catch(console.log)
+      .then(json => {
+        this.setState({
+          userData: json,
+          isLoaded: true,
+        })
+      }).catch((err) => {
+        console.log(err);
+      });
   }
   render() {
+    const { isLoaded } = this.state;
+    if (!isLoaded)
+      return <div><Loader type="Rings" color="#06d7d9" height={80} width={80} /></div>;
+    const users = Object.keys(this.state.userData);
+    console.log(users);
+    let newUser = users.map((user, i) => {
+      return (
+        <div>
+          <div className="card">
+            <div>
+              <div className="card">
+                <div className="card-body">
+                  <h5 className="card-title">{this.state.userData.name}</h5>
+                  <h6 className="card-subtitle mb-2 text-muted">{this.state.userData.email}</h6>
+                  <h6 className="card-subtitle mb-2 text-muted">{this.state.userData.address}</h6>
+                  <h6 className="card-subtitle mb-2 text-muted">{this.state.userData.bio}</h6>
+                  <h6 className="card-subtitle mb-2 text-muted">{this.state.userData.image}</h6>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )
+    });
     return (
-      <Form>
-        <FormGroup>
-          <label htmlFor="#username">Username</label>
-          <FormInput id="#username" placeholder="Username" />
-        </FormGroup>
-        <FormGroup>
-          <label htmlFor="#password">Password</label>
-          <FormInput type="password" id="#password" placeholder="Password" />
-        </FormGroup>
-      </Form>
+
+      <div>
+
+        <center><h1>Contact List</h1></center>
+        <div >{newUser}</div>
+
+      </div>
     )
   }
 }
+
 
 export default App;
--- src/User.js
+++ src/User.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React from 'react';
 
-class User extends React.Component {
+const User = ({ userData = []}) => {
+  return (
+    <div>
+      <center><h1>Contact List</h1></center>
+      {userData.map((user) => (
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">{user.name}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">{user.email}</h6>
+            <h6 class="card-subtitle mb-2 text-muted">{user.address.streetAddress}</h6>
+            <h6 class="card-subtitle mb-2 text-muted">{user.address}</h6>
+            <h6 class="card-subtitle mb-2 text-muted">{user.bio}</h6>
+            <h6 class="card-subtitle mb-2 text-muted">{user.image}</h6>
+            <p class="card-text">{user.company.catchPhrase}</p>
+          </div>
+        </div>
+      ))}
+    </div>
 
-}
-
+  )
+};
 
 export default User;
